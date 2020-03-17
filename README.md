3fbcab789a8dc7984618d942d2a8b0f9b95e51ba
Himenon <k.himeno314@gmail.com>
2020-03-17 16:48:13 +0900

    add css-loader error pattern

---
+++ README.md
@@ -0,0 +1,12 @@
+# Error: composition is only allowed when selector is single :local class name not in ".label", ".label" is weird
+
+## Usage
+
+```bash
+yarn install
+```
+
+
+```bash
+yarn develop
+```
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,14 @@ import * as ReactDOM from "react-dom";
 const className = require("./index.scss");
 
 const App = () => {
-  return <h1 className={className.app}>Hello world</h1>
+  return (
+    <div>
+      <h1 className={className.app}>Hello world</h1>
+      <p className={className.description}>description</p>
+      <p className={className.description2}>description2</p>
+      <label className={className.label}>Label</label>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
